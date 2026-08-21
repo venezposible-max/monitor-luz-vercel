5d2ddec9ac082b9a02780bc08ebf5aa884d8351f
infogadgets2024 <infogadgets2024@gmail.com>
2026-08-20 21:25:00 -0400

    Fix: servir public/index.html directamente desde Express con la seccion de historial

--- api/index.js
+++ api/index.js
@@ -10,6 +10,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Servir archivos estáticos de /public y la ruta principal /
+app.use(express.static(path.join(__dirname, '../public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+app.get('/estado', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // Memoria compartida en Vercel
 global.devices = global.devices || {};
 global.persistentStore = global.persistentStore || {};
